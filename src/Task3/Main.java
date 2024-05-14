711874919b55debb288572cd07a98bcebb07441d
UladzimirIvanov <ivanouum@gmail.com>
2024-05-14 13:23:56 +0300

    Task3 Не работает удаление объектов, в остальном вроде норм

--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -4,15 +4,36 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student("Vova", "1", 3, 4, 5, 2);
-        Student student2 = new Student("Vlad", "2", 2, 3, 4, 3);
-        Student student3 = new Student("Anton", "2", 2, 2, 2, 2);
+        Student student1 = new Student("Vova", "1b", 1, 4, 5, 2);
+        Student student2 = new Student("Vlad", "2b", 2, 3, 4, 3);
+        Student student3 = new Student("Anton", "2b", 2, 2, 2, 2);
+        Student student4 = new Student("Andrei", "3a", 4, 3, 3, 3);
+        Student student5 = new Student("Igor", "5a", 3, 1, 3, 1);
+        Student student6 = new Student("Yan", "3c", 4, 5, 5, 4);
 
         LinkedList<Student> list = new LinkedList<>();
 
         list.add(student1);
         list.add(student2);
         list.add(student3);
+        list.add(student4);
+        list.add(student5);
+        list.add(student6);
+        System.out.println(list);
+
+        Student.nextCourseStudent(list);
+        System.out.println();
+        Student.printStudents(list, 5);
+        System.out.println();
+        for (int i = 0; i < list.size(); i++) {
+            Student.delStudent(list);
+        }
+        list.remove(Student.test.get(0));
+        //Student.delStudent(list);
+
+        System.out.println();
         System.out.println(list);
     }
+
+
 }
--- src/Task3/Student.java
+++ src/Task3/Student.java
@@ -1,6 +1,6 @@
 package Task3;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
@@ -10,6 +10,7 @@ public class Student {
     public int gradeHistory;
     public int gradeLiterature;
     public int gradeMath;
+    public static ArrayList<Integer> test;
 
     public Student(String name, String group, int course, int gradeHistory, int gradeLiterature, int gradeMath) {
         this.name = name;
@@ -18,6 +19,38 @@ public class Student {
         this.gradeHistory = gradeHistory;
         this.gradeLiterature = gradeLiterature;
         this.gradeMath = gradeMath;
+    }
+
+    public static void printStudents(List<Student> student, int course){
+        for (Student s : student){
+            if (s.course == course){
+                System.out.println(s.name + " обучается на " + course + " курсе");
+            }
+        }
+    }
+
+    public static void nextCourseStudent(List<Student> student){
+        for (Student s : student){
+            if ((s.gradeHistory + s.gradeMath + s.gradeLiterature)/3 >= 3){
+                System.out.println(s.name + " Переведён на следующий курс");
+                s.course = s.course + 1;
+            }
+        }
+    }
+
+    public static void delStudent(List<Student> student){
+        ArrayList<Integer> myList = new ArrayList<>();
+        //Student.test = new int[];
+        for (Student s : student){
+            if ((s.gradeHistory + s.gradeMath + s.gradeLiterature)/3 < 3){
+                System.out.println(s.name + " отчислен");
+                System.out.println(student.indexOf(s));
+                myList.add(student.indexOf(s));
+            }
+        }
+        //String[] arr = myList.toArray(new String[myList.size()]);
+        Student.test = myList;
+        System.out.println(Student.test);
     }
 
     @Override
@@ -29,6 +62,6 @@ public class Student {
                 ", gradeHistory=" + gradeHistory +
                 ", gradeLiterature=" + gradeLiterature +
                 ", gradeMath=" + gradeMath +
-                '}';
+                '}' + "\n";
     }
 }
